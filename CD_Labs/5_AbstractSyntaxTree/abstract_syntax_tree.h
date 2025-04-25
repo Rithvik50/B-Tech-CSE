d7a9ccf982b5f48de89e3b982f7d196ffa7666a6
Rithvik50 <rmuthyalapati@gmail.com>
2025-04-25 13:18:26 +0530

    Update abstract_syntax_tree.h

--- CD_Labs/5_AbstractSyntaxTree/abstract_syntax_tree.h
+++ CD_Labs/5_AbstractSyntaxTree/abstract_syntax_tree.h
@@ -2,7 +2,7 @@ typedef struct expression_node
 {
 	struct expression_node* left;		//pointer to the left child
 	char* val;							//value of the node
-	struct expression_node* right;		// pointer to the right child
+	struct expression_node* right;		//pointer to the right child
 }expression_node;
 
 expression_node* init_exp_node(char* val, expression_node* left, expression_node* right);
